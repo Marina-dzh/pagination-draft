5f0c8c7b712855289fb622ce8f64f74efbe091dd
Maryna Dzuh <maryna.dzuh@gmail.com>
2022-12-16 00:21:43 +0200

    up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 
-
-
-
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import { fetchImages } from './fetchImages'
@@ -43,7 +40,6 @@ function onSearch(e) {
       
       Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
       
-      
     }).catch(error => { Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.') });
 
  
@@ -53,7 +49,6 @@ function onSearch(e) {
 function createPagination(totalHits) {
   console.log(totalHits)
     const pagination = new Pagination(document.getElementById('tui-pagination-container'), {
-     
         totalItems: totalHits,
         itemsPerPage: 10,
         visiblePages: 5,
@@ -77,7 +72,6 @@ function createPagination(totalHits) {
 
  
 function createMarkup(arr) {
-  // refs.load.disabled = false;
   return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `<a class="gallery__item" href="${largeImageURL}"><div class="photo-card">
      
   <div class="thumb"><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy"/></div>
@@ -97,22 +91,4 @@ function createMarkup(arr) {
   </div>
 </div></a>`).join("");
   
-    
 }
-// function onLoad() {
-//   page += 1;
-  
-//   const query = refs.input.value.trim()
-//   console.log("hujhuh")
-//   fetchImages(query,page)
-//     .then(data => {
-//       refs.list.insertAdjacentHTML("beforeend", createMarkup(data.hits));
-//       gallery.refresh();
-//       if (data.totalHits < 40 * page) {
-//         refs.load.disabled = true;
-//       Notiflix.Notify.info('We are sorry, but you have reached the end of search results.')}
-//     }).catch(console.error())
-  
-//   }
-  
-
